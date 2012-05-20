7586814324f42fe12dab27806110f0f45d110274
Kartik Singhal <kartiksinghal@gmail.com>
2012-05-20 13:46:39 +0530

    added a basic event model

---
+++ eventdjango/eventdjango/models.py
@@ -0,0 +1,34 @@
+from django.db import models
+
+
+class Event(models.Model):
+    def __unicode__(self):
+        return self.title
+    title = models.CharField("Title", max_length=50)
+    subtitle = models.CharField("Subtitle", max_length=100, blank=True)
+    slogan = models.CharField("Slogan", max_length=255, blank=True) # or tagline
+    about = models.TextField("About") # or description
+    start_date = models.DateTimeField("Start Date Time")
+    end_date = models.DateTimeField("End Date Time")
+    organizer = models.CharField("Organizer", max_length=255) # can be an institution, company, person, or group
+    email = models.EmailField("Contact Email", max_length=75)
+    phone = models.CharField("Phone Number", max_length=20, blank=True)
+    homepage = models.URLField(blank=True)
+    logo = models.ImageField("Logo", upload_to='photos/events', blank=True)
+    venue = models.CharField("Venue", max_length=255)
+
+class Conference(Event):
+
+
+class Hackathon(Event):
+
+
+class Talk(Event):
+    sp_name = models.CharField("Speaker Name", max_length=100)
+    sp_email = models.EmailField("Speaker Email", max_length=75)
+    sp_phone = models.CharField("Speaker Phone", max_length=20, blank=True)
+    photo = models.ImageField("Logo", upload_to='photos/events', blank=True)
+
+# Commented because the event can be general which doesn't fit into any subclasses
+#    class Meta:
+#        abstract = False
