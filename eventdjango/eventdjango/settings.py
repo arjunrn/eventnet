ba324f2b1deee65c4fe1ccfa48c9d112f253c570
Kartik Singhal <kartiksinghal@gmail.com>
2012-05-20 16:46:42 +0530

    fix for serving static files

--- eventdjango/eventdjango/settings.py
+++ eventdjango/eventdjango/settings.py
@@ -68,6 +68,7 @@ STATIC_URL = '/static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
+    path.abspath('static'),
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
@@ -107,7 +108,7 @@ ROOT_URLCONF = 'eventdjango.urls'
 WSGI_APPLICATION = 'eventdjango.wsgi.application'
 
 TEMPLATE_DIRS = (
-        path.abspath('templates'),
+    path.abspath('templates'),
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
